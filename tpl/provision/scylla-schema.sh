a1c91f1855863ab394ef47b531e273aed4a813d5
Rafal Jeczalik <rjeczalik@gmail.com>
2018-11-18 05:50:57 +0100

    retry setting up credentials

--- tpl/provision/scylla-schema.sh
+++ tpl/provision/scylla-schema.sh
@@ -6,8 +6,11 @@ while ! cqlsh -u cassandra -p cassandra -e "DESCRIBE CLUSTER;" &>/dev/null; do
 	sleep 5
 done
 
-cqlsh -u cassandra -p cassandra -e "CREATE USER IF NOT EXISTS ${admin} WITH PASSWORD '${admin_password}' SUPERUSER;"
-cqlsh -u ${admin} -p "${admin_password}" -e "DROP USER cassandra;"
-
-cqlsh -u ${admin} -p "${admin_password}" -e "ALTER KEYSPACE system_auth WITH REPLICATION = {'class' : 'NetworkTopologyStrategy', '${dc}': ${system_auth_replication}};"
-cqlsh -u ${admin} -p "${admin_password}" -e "CREATE USER IF NOT EXISTS ${user} WITH PASSWORD '${user_password}' SUPERUSER;"
+while sleep 5; do
+	if cqlsh -u cassandra -p cassandra -e "CREATE USER IF NOT EXISTS ${admin} WITH PASSWORD '${admin_password}' SUPERUSER;" &&
+		cqlsh -u ${admin} -p "${admin_password}" -e "ALTER KEYSPACE system_auth WITH REPLICATION = {'class' : 'NetworkTopologyStrategy', '${dc}': ${system_auth_replication}};" &&
+		cqlsh -u ${admin} -p "${admin_password}" -e "CREATE USER IF NOT EXISTS ${user} WITH PASSWORD '${user_password}' SUPERUSER;" &&
+		cqlsh -u ${admin} -p "${admin_password}" -e "DROP USER cassandra;"; then
+		break
+	fi
+done
